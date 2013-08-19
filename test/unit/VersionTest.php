8af57fe111ae451bda98f4f18b399fde7e451655
Levi Morrison <levim@php.net>
2013-08-19 00:21:51 -0600

    Improved test cases, now at 100% test coverage coverage.

--- test/unit/VersionTest.php
+++ test/unit/VersionTest.php
@@ -22,47 +22,47 @@ class VersionTest extends \PHPUnit_Framework_TestCase {
     }
     function provideLessThanCases() {
         return [
-            '1.0.0 < 2.0.0' => [
+            '1.0.0 vs 2.0.0' => [
                 [1, 0, 0, [], []],
                 [2, 0, 0, [], []],
             ],
-            '2.0.0 < 2.1.0' => [
+            '2.0.0 vs 2.1.0' => [
                 [2, 0, 0, [], []],
                 [2, 1, 0, [], []],
             ],
-            '2.1.0 < 2.1.0' => [
-                [2, 0, 0, [], []],
+            '2.1.0 vs 2.1.1' => [
                 [2, 1, 0, [], []],
+                [2, 1, 1, [], []],
             ],
-            '1.0.0-alpha < 1.0.0' => [
+            '1.0.0-alpha vs 1.0.0' => [
                 [1, 0, 0, ['alpha'], []],
                 [1, 0, 0, [], []],
             ],
-            '1.0.0-alpha < 1.0.0-alpha.1' => [
+            '1.0.0-alpha vs 1.0.0-alpha.1' => [
                 [1, 0, 0, ['alpha'], []],
                 [1, 0, 0, ['alpha','1'], []],
             ],
-            '1.0.0-alpha.1 < 1.0.0-alpha.beta' => [
+            '1.0.0-alpha.1 vs 1.0.0-alpha.beta' => [
                 [1, 0, 0, ['alpha','1'], []],
                 [1, 0, 0, ['alpha','beta'], []],
             ],
-            '1.0.0-alpha.beta < 1.0.0-beta' => [
+            '1.0.0-alpha.beta vs 1.0.0-beta' => [
                 [1, 0, 0, ['alpha','beta'], []],
                 [1, 0, 0, ['beta'], []],
             ],
-            '1.0.0-beta < 1.0.0-beta.2' => [
+            '1.0.0-beta vs 1.0.0-beta.2' => [
                 [1, 0, 0, ['beta'], []],
                 [1, 0, 0, ['beta','2'], []],
             ],
-            '1.0.0-beta.2 < 1.0.0-beta.11' => [
+            '1.0.0-beta.2 vs 1.0.0-beta.11' => [
                 [1, 0, 0, ['beta','2'], []],
                 [1, 0, 0, ['beta','11'], []],
             ],
-            '1.0.0-beta.11 < 1.0.0-rc.1' => [
+            '1.0.0-beta.11 vs 1.0.0-rc.1' => [
                 [1, 0, 0, ['beta','11'], []],
-                [1, 0, 0, ['rc','1'], []],
+                [1, 0, 0, ['rc.1'], []],
             ],
-            '1.0.0-rc.1 < 1.0.0' => [
+            '1.0.0-rc.1 vs 1.0.0' => [
                 [1, 0, 0, ['rc','1'], []],
                 [1, 0, 0, [], []],
             ],
@@ -95,8 +95,12 @@ class VersionTest extends \PHPUnit_Framework_TestCase {
     function testLessThanCases($aArray, $bArray) {
         $a = $this->makeVersion($aArray);
         $b = $this->makeVersion($bArray);
+
         $actual = Version::compare($a, $b);
         $this->assertLessThan(0, $actual);
+
+        $actual = Version::compare($b, $a);
+        $this->assertGreaterThan(0, $actual);
     }
 
 }
